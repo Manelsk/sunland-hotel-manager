882ac8cd827090f6757bfb6dacfd4e04c639338d
Emanuel Victor <barrosemanuelvictor824@gmail.com>
2026-06-10 19:22:28 -0300

    QuartoSimples gets e sets, construtor

--- models/QuartoSimples.java
+++ models/QuartoSimples.java
@@ -1,5 +1,17 @@
 public class QuartoSimples extends Quarto {
     
-    private boolean temArCondicionado;
+    private boolean possuiArCondicionado;
     
+    public void setPossuiArCondicionado(boolean possuiArCondicionado) {
+        this.possuiArCondicionado = possuiArCondicionado;
+    }
+
+    public boolean temArCondicionado() {
+        return possuiArCondicionado;
+    }
+
+    public QuartoSimples(int numero, double precoBase, int capacidade, boolean possuiArCondicionado) {
+        super(numero, precoBase, capacidade, "disponivel");
+        this.possuiArCondicionado = possuiArCondicionado;
+    }
 }
