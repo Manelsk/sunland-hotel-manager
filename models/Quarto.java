7d8f950fc2e16e374d4968e75715feb1d512744f
Emanuel Victor <barrosemanuelvictor824@gmail.com>
2026-06-10 18:31:16 -0300

    attclassquarto

--- models/Quarto.java
+++ models/Quarto.java
@@ -1,19 +1,49 @@
-public class Quarto {
+public abstract class Quarto implements Reservavel {
     private int numero;
     private double precoBase;
     private int capacidade;
     private String status;
 
-    public Quarto(String nome, double preco) {
-        this.nome = nome;
-        this.preco = preco;
+    public int getNumero(){
+        return numero;
     }
 
-    public String getNome() {
-        return nome;
+    public double getPrecoBase() {
+        return precoBase;
     }
 
-    public double getPreco() {
-        return preco;
+    public int getCapacidade() {
+        return capacidade;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean estaDisponivel() {
+        if (status == null) {
+            return false;
+        }
+        return "disponivel".equals(status);
+    }
+
+    public abstract double calcularDiaria();
+
+    public abstract String exibirDescricao();
 }
